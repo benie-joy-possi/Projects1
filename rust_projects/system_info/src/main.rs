d55dee34e1129b48a77e64337324ca1d73e23ff0
benie-joy-possi <beniejoypossi@gmail.com>
2025-03-13 07:26:14 +0100

    created a main.rs that gets system informations and display them or return an error if could m=not get it

---
+++ rust_projects/system_info/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use sys_info;
+fn main() {
+    match env::current_dir() {
+        Ok(path) =>   println!(" The current working directory is: {:?}",path),
+        Err(_) => println!("we could not get the current working directory")    
+    }
+    match sys_info::os_type() {
+        Ok(os_name) => println!("the OS name is: {}", os_name),
+        Err(_) => println!("could not get the operatting system name")
+        
+    }
+    match sys_info::cpu_num() {
+        Ok(cpu_num) => println!("This computer has {} cpu cores", cpu_num),
+        Err(_) => println!("could not get number of cpu cores")
+        
+    }
+   
+}
